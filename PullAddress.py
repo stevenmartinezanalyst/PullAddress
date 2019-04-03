c28f8d92217e8a5edf547ed1b77a04dc12b79e16
stevenmartinezanalyst <stevenmartinezanalyst@gmail.com>
2019-04-02 18:23:34 -0700

    Modified script to utilize with statement

--- PullAddress.py
+++ PullAddress.py
@@ -8,22 +8,20 @@ with open ("Diamond Bar, CA.html") as html_file:
 	soup = BeautifulSoup(html_file, 'lxml')
 
 #Create CSV to store the address information
-csv_file = open('Addresses.csv', 'w')
-csvwriter = csv.writer(csv_file)
-count = 0
+with open('Addresses.csv', 'w') as csv_file:
+	csvwriter = csv.writer(csv_file)
+	count = 0
 
-#Loop through the HTML page to gather relevant information to store into the CSV
-for listing in soup.find_all('div', class_='item facility hasRating'):
-	if count == 0:
-		header = ["Name","Street","City","State","ZIP","URL"]
-		csvwriter.writerow(header)
-		count += 1
-	listname = listing.h3.span.text
-	address = listing.find('span', class_='street-address').text
-	locality = listing.find_all('span', class_='locality')[0].text
-	state = listing.find_all('span', class_='locality')[1].text
-	postcode = listing.find('span', class_='postal-code').text
-	url = listing.find('a', href=True)
-	csvwriter.writerow([listname,address,locality,state,postcode,url['href']])
-csv_file.close()
-html_file.close()
+	#Loop through the HTML page to gather relevant information to store into the CSV
+	for listing in soup.find_all('div', class_='item facility hasRating'):
+		if count == 0:
+			header = ["Name","Street","City","State","ZIP","URL"]
+			csvwriter.writerow(header)
+			count += 1
+		listname = listing.h3.span.text
+		address = listing.find('span', class_='street-address').text
+		locality = listing.find_all('span', class_='locality')[0].text
+		state = listing.find_all('span', class_='locality')[1].text
+		postcode = listing.find('span', class_='postal-code').text
+		url = listing.find('a', href=True)
+		csvwriter.writerow([listname,address,locality,state,postcode,url['href']])
